40cb33dd5fe4d4a50b3aa14e395c7fcf2772f0fb
evad37 <evad37@gmail.com>
2018-01-30 18:55:44 +0800

    QR code generator

--- web/about.php
+++ web/about.php
@@ -1,12 +1,13 @@
 <?php
-	echo_html_top();
 	echo makeHeading("Free Knowledge Portal");
-	echo makeSubheading("Stable urls that showcase a Wikidata item's sitelinks, and related items &ndash; suitable for QR codes.");
+	echo makeSubheading("Stable urls that showcase a Wikidata item's sitelinks, and related items.");
 	if ( strlen($item_id) > 0 ) {
 		echo("Bad item code: Must be a number prefixed with \"Q\".<hr>");
 	}
-	echo("Try <a href={$self}/Q100>{$self}/Q100</a> (or any other Wikidata item id) to see this tool in action.<br>");
-	echo("Or try <a href={$self}/Q100>{$self}/Q100/fr</a> (or any other language) to see it in another language.<hr>");
-	echo makefooter();
-	echo "</body></html>";
+	echo (
+		"<p style='text-align:center'>
+		Try <a href={$self}/Q100>{$self}/Q100</a> (or any other Wikidata item id) to see this tool in action.<br>
+		Or try <a href={$self}/Q100>{$self}/Q100/fr</a> (or any other language) to see it in another language.
+		</p>"
+	);
 ?>
