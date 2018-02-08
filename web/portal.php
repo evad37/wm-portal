3ad5d4d362816e662114f083bbd09c629cac0746
evad37 <evad37@gmail.com>
2018-02-08 14:03:20 +0800

    Specify site order
    Fixes #19

--- web/portal.php
+++ web/portal.php
@@ -1,17 +1,6 @@
 <?php
 
-$item_data  = $api->lookupItemData($item_id, $lang_code, $sites);
-$item_label = getDeepData($item_data, ["labels", $lang_code, "value"], "({$item_id}: {$i18n['nolabel']})");
-$item_desc  = getDeepData($item_data, ["descriptions", $lang_code, "value"]);
-$sitelinks  = addReasonator( getDeepData($item_data, ["sitelinks"], []),  $item_id, $lang_code);
-$related_items = $api->lookupMultipleItemsData(
-	$api->lookupRelatedItemIds($item_id),
-	$lang_code
-);
-$nearby_items  = $api->lookupMultipleItemsData(
-	$api->lookupNearbyItemIds( $api->lookupCoords($item_id) ),
-	$lang_code
-);
+require "inc/items.php";
 
 echo_html_top($item_label);
 
